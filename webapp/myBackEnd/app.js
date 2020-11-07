1083bce55bd435cb9af7545b31ee8cd9b4373ead
kotunde <tundekoncsard3566@gmail.com>
2020-11-07 16:20:05 +0200

    backend-mysql connection

--- webapp/myBackEnd/app.js
+++ webapp/myBackEnd/app.js
@@ -5,6 +5,33 @@ const express = require("express");
  app.get("/", (req,res) => res.send("This is the root page!"));
  app.get("/spec", (req,res) => res.send("This is a specific page"));
 
+ app.get("/greeting",(req,res) => res.send("Hello from the backend"));
+
+ var mysql = require('mysql');
+
+ var con = mysql.createConnection({
+   host: "localhost",
+   user: "tunde",
+   password: "incorrect",
+   database: "webapp"
+ });
+ 
+ con.connect(function(err) {
+   if (err) throw err;
+   else
+   console.log("Connected!");
+ });
+
+con.query('SELECT * FROM users', function(err, rows, fields){
+    if (err){
+        console.log('Query error')
+        console.log(err);
+    } else {
+        console.log('The solution is:',rows)
+    }
+});
+
+
  app.get("/users",(req,res) => res.send("Hello from the backend"));
 
  app.listen(port, () => console.log(`Example app listening on port ${port}!`));
